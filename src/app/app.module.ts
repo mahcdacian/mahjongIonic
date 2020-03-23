522918f27ed4b35a1f39846ab22549c819ba0101
Rupesh Dhadiwal <dhadiwalrupesh7@gmail.com>
2020-03-23 09:39:39 +0530

    background image changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,14 @@ import { FormsModule } from '@angular/forms';
 import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
 
 const firebaseConfig = {
-  // add config and remove it before promoting code same goes with environment.ts
- 
+  apiKey: 'AIzaSyBeUW5Jj2O4Vuk4-6qADFUmfJe4A0_xoqU',
+  authDomain: 'mahjong-c2571.firebaseapp.com',
+  databaseURL: 'https://mahjong-c2571.firebaseio.com',
+  projectId: 'mahjong-c2571',
+  storageBucket: 'mahjong-c2571.appspot.com',
+  messagingSenderId: '195224008416',
+  appId: '1:195224008416:web:fb7f5acb9212cd21248285',
+  measurementId: 'G-WBCPTTD8DC'
 };
 
 @NgModule({
